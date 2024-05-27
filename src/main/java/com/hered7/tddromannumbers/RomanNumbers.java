cb61d0c39a06dd2464afbaa4f37e4af8ca8beb13
dani heredia <daniel.heredia@manomano.com>
2024-05-27 06:51:47 +0200

    TDD-Basic cases test to solve

--- src/main/java/com/hered7/tddromannumbers/RomanNumbers.java
+++ src/main/java/com/hered7/tddromannumbers/RomanNumbers.java
@@ -2,6 +2,9 @@ package com.hered7.tddromannumbers;
 
 public class RomanNumbers {
     public String convertToRoman(int naturalNumber) {
+        if(naturalNumber == 2){
+            return "II";
+        }
         return "I";
     }
 }
--- src/test/java/com/hered7/tddromannumbers/RomanNumbersApplicationTests.java
+++ src/test/java/com/hered7/tddromannumbers/RomanNumbersApplicationTests.java
@@ -17,4 +17,53 @@ class RomanNumbersApplicationTests {
         assertEquals(roman, "I");
     }
 
+    @Test
+    void numberTwotoRoman() {
+        String roman = romanNumbers.convertToRoman(2);
+
+        assertEquals(roman, "II");
+    }
+
+    @Test
+    void numberFourtoRoman() {
+        String roman = romanNumbers.convertToRoman(4);
+
+        assertEquals(roman, "IV");
+    }
+    @Test
+    void numberFivetoRoman() {
+        String roman = romanNumbers.convertToRoman(5);
+
+        assertEquals(roman, "V");
+    }
+    @Test
+    void numberTentoRoman() {
+        String roman = romanNumbers.convertToRoman(10);
+
+        assertEquals(roman, "X");
+    }
+    @Test
+    void numberFiftytoRoman() {
+        String roman = romanNumbers.convertToRoman(50);
+
+        assertEquals(roman, "L");
+    }
+    @Test
+    void numberHundredtoRoman() {
+        String roman = romanNumbers.convertToRoman(100);
+
+        assertEquals(roman, "C");
+    }
+    @Test
+    void numberFiveHundredtoRoman() {
+        String roman = romanNumbers.convertToRoman(500);
+
+        assertEquals(roman, "D");
+    }
+    @Test
+    void numberThousandtoRoman() {
+        String roman = romanNumbers.convertToRoman(1000);
+
+        assertEquals(roman, "M");
+    }
 }
