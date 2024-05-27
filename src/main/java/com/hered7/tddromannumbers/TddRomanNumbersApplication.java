dfd7191733fc7aedb320b1dccefcf6012afad611
dani heredia <daniel.heredia@manomano.com>
2024-05-27 06:17:38 +0200

    Initial commit

---
+++ src/main/java/com/hered7/tddromannumbers/TddRomanNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.hered7.tddromannumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRomanNumbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddRomanNumbersApplication.class, args);
+    }
+
+}
